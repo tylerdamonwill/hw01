50fedebf049d233c7991bd7e7dbb6b4d2e9da13d
williams2019 <williams13@grinnell.edu>
2016-09-04 17:33:29 -0500

    Complete Caesar Cipher

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class CaesarCipher{
 	public static void main(String[] args){
+
 		Scanner in = new Scanner(System.in);
 
 		System.out.println("This program encrypts and decrypts messages using the Caeser Cipher.\n"
@@ -12,27 +13,49 @@ public class CaesarCipher{
 		if(response.equals("encode")){
 			System.out.println("Enter the string to encode:");
 			response = in.nextLine();
-			for(int n = 0; n < 26; n++){
-				System.out.print("n = " + n + ": ");
-				for(int i = 0; i < response.length(); i++){
-					char letter = response.charAt(i);
-					int num = (int) letter;
-					num = ((((num + n) - 97) % 26) + 97);
-					if (i < (response.length() - 1)){
-						System.out.print((char)num);}
-					else {System.out.println((char)num);}	
-				}
-			}
+			encodeMessage(response);
 		}
-		/*for(n = 0; n < 26; n++){
-
-            }*/
 		else if (response.equals("decode")){
-
+			System.out.println("Enter the string to encode:");
+			response = in.nextLine();
+			decodeMessage(response);
 		}
 		else{
 			System.out.println("Valid options are 'encode' or 'decode'");
 			System.exit(0);
 		}
+		in.close();
+	}
+
+	public static void encodeMessage (String message){
+		for(int n = 0; n < 26; n++){
+			System.out.print("n = " + n + ": ");
+			for(int i = 0; i < message.length(); i++){
+				char letter = message.charAt(i);
+				int num = (int) letter;
+				num = ((((num + n) - 97) % 26) + 97);
+				if (i < (message.length() - 1)){
+					System.out.print((char)num);
+				}
+				else {System.out.println((char)num);
+				}	
+			}
+		}
+	}
+	public static void decodeMessage (String message){
+		for(int n = 0; n < 26; n++){
+			System.out.print("n = " + n + ": ");
+			for(int i = 0; i < message.length(); i++){
+				char letter = message.charAt(i);
+				int num = (int) letter;
+				num = ((((num - n) - 71) % 26) + 97);
+				if (i < (message.length() - 1)){
+					System.out.print((char)num);
+				}
+				else {System.out.println((char)num);
+				}	
+			}
+		}
 	}
 }
+
